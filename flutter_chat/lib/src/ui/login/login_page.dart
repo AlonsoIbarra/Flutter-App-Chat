72c183d0ae0d041467d8441af90ba97c6b065b10
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-21 20:16:09 -0800

    Add page for login

--- flutter_chat/lib/src/ui/login/login_page.dart
+++ flutter_chat/lib/src/ui/login/login_page.dart
@@ -1,38 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/src/config/utils/app_colors.dart';
+import 'package:flutter_chat/src/config/utils/app_image_routs.dart';
+import 'package:flutter_chat/src/ui/login/login_controller.dart';
+import 'package:flutter_chat/src/ui/login/widgets/login_form.dart';
 import 'package:sizer/sizer.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  LoginPage({
+    Key? key,
+    LoginController? loginController,
+  })  : loginController = loginController ?? LoginController(),
+        super(key: key);
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
+  LoginController loginController;
 
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter chat app'),
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 3.h,
-          ),
-          const Text('Correo electrónico'),
-          SizedBox(
-            height: 2.h,
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 3.h),
-            child: const TextField(
-              decoration: InputDecoration(
-                hintText: 'Correo electrónico',
+      body: SizedBox(
+        width: double.infinity,
+        height: 100.h,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: EdgeInsets.only(right: 70.w),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 15.h,
+                      width: 15.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(22.w)),
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 6.h,
+                        left: 7.w,
+                      ),
+                      child: Text(
+                        'login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+              Image.asset(
+                AppImageRoutes.loginImage,
+                width: 40.w,
+                height: 20.h,
+              ),
+              LoginForm(
+                loginController: loginController,
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
