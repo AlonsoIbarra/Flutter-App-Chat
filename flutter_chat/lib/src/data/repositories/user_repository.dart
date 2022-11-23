9569389d820f6d105a26cc82783cb715ec27fa5f
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-22 21:11:39 -0800

    create user repository

---
+++ flutter_chat/lib/src/data/repositories/user_repository.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_chat/src/domain/models/user/user.dart';
+
+import 'package:flutter_chat/src/domain/errors/http_errors/http_request_error.dart';
+
+import 'package:flutter_chat/src/domain/either/either.dart';
+
+import '../../domain/abstract_repositories/abstractuser_repository.dart';
+import '../mixins/handle_http_request_error.dart';
+import '../services/remote/user_service.dart';
+
+class UserRepository
+    with HandleHttpRequestErrorMixin
+    implements AbstractUserRepository {
+  final UserService _userService;
+
+  UserRepository(this._userService);
+  @override
+  Future<Either<HttpRequestError, bool>> createUser(User user) async {
+    final result = await _userService.create(user);
+    if (result.error == null) {
+      return Right(result.data!);
+    }
+    return handleHttpRequestError(result);
+  }
+}
---
+++ flutter_chat/lib/src/domain/abstract_repositories/abstractuser_repository.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_chat/src/domain/models/user/user.dart';
+
+import '../either/either.dart';
+import '../errors/http_errors/http_request_error.dart';
+
+abstract class AbstractUserRepository {
+  Future<Either<HttpRequestError, bool>> createUser(User user);
+}
