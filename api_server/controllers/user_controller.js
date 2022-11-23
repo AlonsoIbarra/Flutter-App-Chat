c9d33feaa5abf19194298c3b31e101f2e7b5d46c
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-23 00:10:54 -0800

    create login function

--- api_server/controllers/user_controller.js
+++ api_server/controllers/user_controller.js
@@ -1,37 +1,88 @@
-const user = require("../models/user");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const { token } = require("morgan");
+const jwt = require("jsonwebtoken");
+const keys = require("../config/keys");
 
 module.exports = {
-    async getAll(request, result, next ){
+    async getAll(request, response, next ){
         try {
-            const data = await user.getAll();
-            console.log(`users: ${data}`);
-            return result.status(200).json({
+            const data = await User.getAll();
+            return response.status(200).json({
                 success: true,
                 data: data
             });
         } catch (error) {
             console.log(`error: ${error}`);
-            return result.status(501).json({
+            return response.status(501).json({
                 success: false,
                 message: error.message
             });
         }
     },
-    async create(request, result, next ){
+    async create(request, response, next ){
         try {
             const user_data = request.body;
-            const data = await user.create(user_data);
-            console.log(`user id: ${data.id}`);
-            return result.status(201).json({
+            const data = await User.create(user_data);
+            return response.status(201).json({
                 success: true,
                 data: data.id
             });
         } catch (error) {
             console.log(`error: ${error}`);
-            return result.status(501).json({
+            return response.status(501).json({
                 success: false,
                 message: error.message
             });
         }
+    },
+    async login(request, response, next){
+        try {
+            const email = request.body.email;
+            const password = request.body.password;
+            const match_user = await User.findByEmail(email);
+            if(!match_user){
+                return response.status(400).json({
+                    success: false,
+                    message: 'User not found'
+                });
+            }
+            const isValid = await bcrypt.compare(password, match_user.password);
+            if( isValid){
+                const token = jwt.sign(
+                    {
+                        id: match_user.id,
+                        email: match_user.email,
+                    },
+                    keys.secretOrKey,
+                    {
+                        // expiresIn: 
+                    }
+                );
+                const data = {
+                    id: match_user.id,
+                    name: match_user.name,
+                    email: match_user.email,
+                    phone: match_user.phone,
+                    image: match_user.image,
+                    session_token: `JWT ${token}`
+                };
+                return response.status(200).json({
+                    success: true,
+                    data: data
+                });
+            }
+            return response.status(401).json({
+                success: false,
+                message: "Wrong password"
+            });
+        } catch (error) {
+            console.log(`error: ${error}`);
+            return response.status(501).json({
+                success: false,
+                message: error.message
+            });
+        }
+
     }
 }
