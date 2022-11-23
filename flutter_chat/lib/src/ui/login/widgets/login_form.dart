ead13a02d97e35ce40806160553039462e88d7b5
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-23 14:40:37 -0800

    Add validations for login form

--- flutter_chat/lib/src/ui/login/widgets/login_form.dart
+++ flutter_chat/lib/src/ui/login/widgets/login_form.dart
@@ -12,116 +12,139 @@ class LoginForm extends StatelessWidget {
     required this.loginController,
   }) : super(key: key);
   LoginController loginController;
+  final GlobalKey<FormState> _loginKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: loginController.emailController,
-            style: TextStyle(
-              color: AppColors.textColor,
+    return Form(
+      key: _loginKey,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
             ),
-            decoration: InputDecoration(
-              hintText: 'Correo electrónico',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.email,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: loginController.passwordController,
-            obscureText: true,
-            style: TextStyle(
-              color: AppColors.textColor,
-            ),
-            decoration: InputDecoration(
-              hintText: 'Contraseña',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        SizedBox(
-          width: 70.w,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                  5.w,
-                ),
-              ),
-              padding: EdgeInsets.symmetric(
-                vertical: 3.w,
-              ),
-            ),
-            onPressed: () => loginController.login(),
-            child: Text(
-              'Continuar',
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Ingrese correo electrónico';
+                }
+                if (!GetUtils.isEmail(value ?? '')) {
+                  return 'Ingrese correo electrónico valido';
+                }
+                return null;
+              },
+              controller: loginController.emailController,
               style: TextStyle(
                 color: AppColors.textColor,
               ),
-            ),
-          ),
-        ),
-        SizedBox(height: 5.h),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('No tienes cuenta?'),
-            SizedBox(
-              width: 1.w,
-            ),
-            GestureDetector(
-              onTap: () {
-                Get.toNamed(
-                  Routes.register,
-                );
-              },
-              child: const Text(
-                'Registrate',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.brown,
+              decoration: InputDecoration(
+                hintText: 'Correo electrónico',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
                 ),
               ),
             ),
-          ],
-        ),
-      ],
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Ingrese contraseña';
+                }
+                return null;
+              },
+              controller: loginController.passwordController,
+              obscureText: true,
+              style: TextStyle(
+                color: AppColors.textColor,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Contraseña',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          SizedBox(
+            width: 70.w,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                    5.w,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(
+                  vertical: 3.w,
+                ),
+              ),
+              onPressed: () {
+                if (_loginKey.currentState?.validate() ?? false) {
+                  loginController.login();
+                }
+              },
+              child: Text(
+                'Continuar',
+                style: TextStyle(
+                  color: AppColors.textColor,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 5.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('No tienes cuenta?'),
+              SizedBox(
+                width: 1.w,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.toNamed(
+                    Routes.register,
+                  );
+                },
+                child: const Text(
+                  'Registrate',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
