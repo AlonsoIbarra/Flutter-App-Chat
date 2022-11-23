4c69bce401f2f6fbfb8e183bb33684c82c5e294c
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-22 22:31:54 -0800

    Validate fields in user register form

--- flutter_chat/lib/src/ui/register/widgets/register_form.dart
+++ flutter_chat/lib/src/ui/register/widgets/register_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/src/config/utils/app_colors.dart';
 import 'package:flutter_chat/src/ui/register/register_controller.dart';
+import 'package:get/utils.dart';
 import 'package:sizer/sizer.dart';
 
 class RegisterForm extends StatelessWidget {
@@ -9,208 +10,263 @@ class RegisterForm extends StatelessWidget {
     required this.registerController,
   }) : super(key: key);
   RegisterController registerController;
+  final GlobalKey<FormState> registerFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: registerController.nameController,
-            style: TextStyle(
-              color: AppColors.textColor,
+    return Form(
+      key: registerFormKey,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
             ),
-            decoration: InputDecoration(
-              hintText: 'Nombre',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.person,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: registerController.userController,
-            style: TextStyle(
-              color: AppColors.textColor,
-            ),
-            decoration: InputDecoration(
-              hintText: 'Usuario',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.person,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: registerController.phoneController,
-            keyboardType: TextInputType.phone,
-            style: TextStyle(
-              color: AppColors.textColor,
-            ),
-            decoration: InputDecoration(
-              hintText: 'Teléfono',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.phone,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: registerController.emailController,
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              color: AppColors.textColor,
-            ),
-            decoration: InputDecoration(
-              hintText: 'Correo electrónico',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.email,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: registerController.passwordController,
-            obscureText: true,
-            style: TextStyle(
-              color: AppColors.textColor,
-            ),
-            decoration: InputDecoration(
-              hintText: 'Contraseña',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: AppColors.primaryOpacityColor,
-            borderRadius: BorderRadius.circular(5.w),
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: TextField(
-            controller: registerController.passwordConfirmController,
-            obscureText: true,
-            style: TextStyle(
-              color: AppColors.textColor,
-            ),
-            decoration: InputDecoration(
-              hintText: 'Confirmar contraseña',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(5.w),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: AppColors.primaryColor,
-              ),
-              hintStyle: TextStyle(
-                color: AppColors.primaryColorDark,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(
-          height: 2.h,
-        ),
-        SizedBox(
-          width: 70.w,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.primaryColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                  5.w,
-                ),
-              ),
-              padding: EdgeInsets.symmetric(
-                vertical: 3.w,
-              ),
-            ),
-            onPressed: () => registerController.register(),
-            child: Text(
-              'Registrarse',
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Ingrese nombre';
+                }
+                return null;
+              },
+              controller: registerController.nameController,
               style: TextStyle(
                 color: AppColors.textColor,
               ),
+              decoration: InputDecoration(
+                hintText: 'Nombre',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
+                ),
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 5.h),
-      ],
+          SizedBox(
+            height: 2.h,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Ingrese usuario';
+                }
+                return null;
+              },
+              controller: registerController.userController,
+              style: TextStyle(
+                color: AppColors.textColor,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Usuario',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Ingrese teléfono';
+                }
+                return null;
+              },
+              controller: registerController.phoneController,
+              keyboardType: TextInputType.phone,
+              style: TextStyle(
+                color: AppColors.textColor,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Teléfono',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.phone,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Ingrese correo electrónico';
+                }
+                if (!GetUtils.isEmail(value ?? '')) {
+                  return 'Ingrese correo electrónico valido';
+                }
+                return null;
+              },
+              controller: registerController.emailController,
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(
+                color: AppColors.textColor,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Correo electrónico',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Ingrese contraseña';
+                }
+                return null;
+              },
+              controller: registerController.passwordController,
+              obscureText: true,
+              style: TextStyle(
+                color: AppColors.textColor,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Contraseña',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryOpacityColor,
+              borderRadius: BorderRadius.circular(5.w),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: TextFormField(
+              validator: (value) {
+                if (GetUtils.isNullOrBlank(
+                        registerController.passwordController.text) ??
+                    false) {
+                  return null;
+                }
+                if (GetUtils.isNullOrBlank(value) ?? false) {
+                  return 'Confirme su contraseña';
+                }
+                if (registerController.passwordController.text != value) {
+                  return 'Las contraseñas no coinciden';
+                }
+                return null;
+              },
+              controller: registerController.passwordConfirmController,
+              obscureText: true,
+              style: TextStyle(
+                color: AppColors.textColor,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Confirmar contraseña',
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(5.w),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: AppColors.primaryColor,
+                ),
+                hintStyle: TextStyle(
+                  color: AppColors.primaryColorDark,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.h,
+          ),
+          SizedBox(
+            width: 70.w,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                    5.w,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(
+                  vertical: 3.w,
+                ),
+              ),
+              onPressed: () async {
+                if (registerFormKey.currentState?.validate() ?? false) {
+                  registerController.register();
+                }
+              },
+              child: Text(
+                'Registrarse',
+                style: TextStyle(
+                  color: AppColors.textColor,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 5.h),
+        ],
+      ),
     );
   }
 }
