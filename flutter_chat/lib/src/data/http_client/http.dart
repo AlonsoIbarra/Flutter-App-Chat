5a9e24d1e158305665d6d4a3c46050ade6e8252f
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-22 21:05:53 -0800

    Add http request class

---
+++ flutter_chat/lib/src/data/http_client/http.dart
@@ -0,0 +1,74 @@
+import 'package:dio/dio.dart';
+
+import 'http_response.dart';
+
+class Http {
+  final Dio dio;
+
+  Http(
+    this.dio,
+  );
+
+  Future<HttpResponse<T>> request<T>(
+    String path, {
+    String method = 'GET',
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? data,
+    Map<String, String?>? headers,
+    T Function(int statusCode, dynamic data)? parser,
+    bool useAuthHeaders = false,
+    CancelToken? cancelToken,
+  }) async {
+    try {
+      headers?.removeWhere((key, value) => value == null);
+
+      final Response response = await dio.request(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: Options(
+          method: method,
+          headers: headers,
+        ),
+        cancelToken: cancelToken,
+      );
+
+      return parser == null
+          ? HttpResponse.sucess<T>(
+              response.statusCode!,
+              response.data,
+            )
+          : HttpResponse.sucess<T>(
+              response.statusCode!,
+              parser(response.statusCode!, response.data),
+            );
+    } on DioError catch (e, s) {
+      String message = e.message;
+      dynamic responseData;
+      int statusCode = 0;
+      if (e.response != null) {
+        statusCode = e.response!.statusCode!;
+        responseData = e.response!.data;
+        message = e.response!.statusMessage!;
+      }
+
+      return HttpResponse.fail<T>(
+        statusCode: statusCode,
+        message: message,
+        data: responseData,
+        failedRefresh: statusCode == 401,
+        type: e.type,
+      );
+    } catch (e, s) {
+      int statusCode = -1;
+
+      return HttpResponse.fail<T>(
+        statusCode: statusCode,
+        message: e.toString(),
+        data: null,
+        failedRefresh: false,
+        type: DioErrorType.other,
+      );
+    }
+  }
+}
