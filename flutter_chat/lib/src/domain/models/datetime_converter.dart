4cb4b6d588a8241134b7035770810f5ab148e72a
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-22 21:07:32 -0800

    Add datetime converter for datetime model fields

--- flutter_chat/lib/src/domain/models/datetime_converter.dart
+++ flutter_chat/lib/src/domain/models/datetime_converter.dart
@@ -16,3 +16,23 @@ class DateTimeConverterNotNull implements JsonConverter<DateTime, String> {
     return object.toUtc().toIso8601String();
   }
 }
+
+class DateTimeConverterNull implements JsonConverter<DateTime?, String?> {
+  const DateTimeConverterNull();
+
+  @override
+  DateTime? fromJson(String? text) {
+    if (text != null) {
+      final timeZoneOffset = DateTime.now().timeZoneOffset;
+      return DateTime.parse(text).add(
+        timeZoneOffset,
+      );
+    }
+    return null;
+  }
+
+  @override
+  String? toJson(DateTime? object) {
+    return object?.toUtc().toIso8601String();
+  }
+}
