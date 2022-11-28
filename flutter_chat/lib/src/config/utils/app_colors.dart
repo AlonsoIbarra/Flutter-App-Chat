1d548fefa80147cf6e2940ee6a8362bf3cfef3e2
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-27 22:53:41 -0600

    Add assets and colors

--- flutter_chat/lib/src/config/utils/app_colors.dart
+++ flutter_chat/lib/src/config/utils/app_colors.dart
@@ -6,4 +6,6 @@ class AppColors {
   static Color? primaryOpacityColor = const Color.fromRGBO(100, 85, 72, 0.4);
   static Color? primaryColorDark = const Color.fromRGBO(100, 86, 60, 50);
   static Color? textColor = const Color.fromRGBO(255, 255, 255, 1);
+  static Color unselectedTab = const Color.fromRGBO(255, 255, 255, 0.5);
+  static Color selectedTab = const Color.fromRGBO(255, 255, 255, 1);
 }
--- flutter_chat/lib/src/config/utils/app_image_routes.dart
+++ flutter_chat/lib/src/config/utils/app_image_routes.dart
@@ -2,4 +2,5 @@ class AppImageRoutes {
   AppImageRoutes._();
   static String loginImage = 'assets/images/delivery.png';
   static String hamburguesa = 'assets/images/hamburguesa.png';
+  static String imageProfile = 'assets/images/user_profile_2.png';
 }
