9f4d1fc583f433348358abd9aef79ac86282ad29
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-27 21:29:42 -0600

    Save user before in GetX storage

--- flutter_chat/lib/src/ui/login/login_controller.dart
+++ flutter_chat/lib/src/ui/login/login_controller.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/src/domain/abstract_repositories/abstract_auth_repository.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../../config/routes/routes.dart';
 
 class LoginController extends GetxController {
   final AbstractAuthRepository _authRepository;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+  final GetStorage _getStorage = GetStorage();
   LoginController(this._authRepository);
   Future<void> login() async {
     final response = await _authRepository.login(
@@ -22,7 +25,10 @@ class LoginController extends GetxController {
         unhandled: () =>
             Get.snackbar('Error', 'Ha ocurrido un error desconocido.'),
       ),
-      (success) => Get.snackbar('OK', 'Login exitoso'),
+      (user) {
+        _getStorage.write('user', user!.toJson());
+        Get.toNamed(Routes.home);
+      },
     );
   }
 }
