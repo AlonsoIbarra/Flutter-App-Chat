039b1ddeae4dd0be3fd66686e970f41a24ba1414
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-28 22:46:31 -0600

    Change user in profile controller to observable value

--- flutter_chat/lib/src/ui/home/widgets/tabs/profile/profile_controller.dart
+++ flutter_chat/lib/src/ui/home/widgets/tabs/profile/profile_controller.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_chat/src/config/routes/routes.dart';
 import 'package:flutter_chat/src/data/services/local/get_storage_service.dart';
+import 'package:flutter_chat/src/data/services/local/multimedia_service.dart';
 import 'package:flutter_chat/src/domain/models/user/user.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
-  final User user = GetStorageService.maybeGetLoggedUser();
+  final MultimediaService multimediaService;
+  final Rx<User> user = GetStorageService.maybeGetLoggedUser().obs;
+
+  ProfileController({MultimediaService? multimediaService})
+      : multimediaService = multimediaService ?? MultimediaService();
 
   void signOut() {
     GetStorageService.deleteGetLoggedUser();
@@ -12,5 +17,10 @@ class ProfileController extends GetxController {
       Routes.login,
       (route) => false,
     );
+  }
+
+  void updateProfileImage() async {
+    final file = await multimediaService.selectImageFromLocalGallery();
+    print("File ${file}");
   }
 }
