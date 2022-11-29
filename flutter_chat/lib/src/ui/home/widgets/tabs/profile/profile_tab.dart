4c4419f6660982e8e3411c9cd0b7d41b7f8aa4b4
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-28 22:47:07 -0600

    Refactor profile tab to observable user

--- flutter_chat/lib/src/ui/home/widgets/tabs/profile/profile_tab.dart
+++ flutter_chat/lib/src/ui/home/widgets/tabs/profile/profile_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/src/config/utils/app_colors.dart';
+import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
 import 'profile_controller.dart';
@@ -22,29 +23,37 @@ class ProfileTab extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(2.w),
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const CircleImageUser(
-                  imageProfilePath:
-                      'https://anthoncode.com/wp-content/uploads/2020/01/logo-batman-old.png',
-                ),
-                ListTile(
-                  leading: const Icon(Icons.person),
-                  title: const Text('Nombre del usuario'),
-                  subtitle: Text(profileController.user.name),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.email),
-                  title: const Text('Email'),
-                  subtitle: Text(profileController.user.email),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.phone),
-                  title: const Text('Telefono'),
-                  subtitle: Text(profileController.user.phone),
-                ),
-              ],
+            child: Obx(
+              () => Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 15.h,
+                    child: GestureDetector(
+                      onTap: () => profileController.updateProfileImage(),
+                      child: CircleImageUser(
+                        profileUserImage:
+                            profileController.user.value.image ?? '',
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.person),
+                    title: const Text('Nombre del usuario'),
+                    subtitle: Text(profileController.user.value.name),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.email),
+                    title: const Text('Email'),
+                    subtitle: Text(profileController.user.value.email),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.phone),
+                    title: const Text('Telefono'),
+                    subtitle: Text(profileController.user.value.phone),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
