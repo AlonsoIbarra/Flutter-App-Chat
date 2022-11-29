30461bae8ddfdb82c80ef5bd7a92686c9fc555fe
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-11-28 22:33:30 -0600

    change image avatar default asset

--- flutter_chat/lib/src/config/utils/app_image_routes.dart
+++ flutter_chat/lib/src/config/utils/app_image_routes.dart
@@ -2,5 +2,5 @@ class AppImageRoutes {
   AppImageRoutes._();
   static String loginImage = 'assets/images/delivery.png';
   static String hamburguesa = 'assets/images/hamburguesa.png';
-  static String imageProfile = 'assets/images/user_profile_2.png';
+  static String imageProfileDefault = 'assets/images/user_profile_2.png';
 }
