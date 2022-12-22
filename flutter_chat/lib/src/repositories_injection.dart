078935f798d4029ddd2559ac7801e4efec2a95bc
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2022-12-22 10:41:31 -0600

    Inject message repository

--- flutter_chat/lib/src/repositories_injection.dart
+++ flutter_chat/lib/src/repositories_injection.dart
@@ -1,12 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_chat/src/data/repositories/auth_repository.dart';
+import 'package:flutter_chat/src/data/repositories/messages_repository.dart';
 import 'package:flutter_chat/src/data/repositories/user_repository.dart';
 import 'package:flutter_chat/src/data/services/remote/auth_service.dart';
+import 'package:flutter_chat/src/data/services/remote/message_service.dart';
 import 'package:flutter_chat/src/data/services/remote/user_service.dart';
 import 'package:flutter_chat/src/domain/abstract_repositories/abstract_auth_repository.dart';
 import 'package:flutter_chat/src/domain/abstract_repositories/abstract_user_repository.dart';
 import 'package:get/get.dart';
 import 'data/http_client/http.dart';
+import 'domain/abstract_repositories/abstract_messages_repository.dart';
 
 Future<void> repositoriesInjection({
   required Dio dio,
@@ -25,6 +28,11 @@ Future<void> repositoriesInjection({
       AuthService(http),
     ),
   );
+  Get.lazyPut<AbstractMessagesRepository>(
+    () => MessagesRepository(
+      MessageService(http),
+    ),
+  );
 }
 
 class RepositoriesGetter {
@@ -34,4 +42,6 @@ class RepositoriesGetter {
       Get.find<AbstractUserRepository>();
   static AbstractAuthRepository get authRepository =>
       Get.find<AbstractAuthRepository>();
+  static AbstractMessagesRepository get messagesRepository =>
+      Get.find<AbstractMessagesRepository>();
 }
